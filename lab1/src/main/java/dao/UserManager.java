b52ab64a1badec7d40173af177229b834b26bf2c
HieuNT-2001 <hieuth0109@gmail.com>
2024-10-31 14:56:53 +0700

    lam bai 2 lab 1

--- lab1/src/main/java/dao/UserManager.java
+++ lab1/src/main/java/dao/UserManager.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.util.List;
+import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -12,16 +13,12 @@ import model.User;
 public class UserManager {
 	EntityManagerFactory factory = Persistence.createEntityManagerFactory("PolyOE");
 	EntityManager em = factory.createEntityManager();
+	Scanner scanner = new Scanner(System.in);
 
 	public void finalAll() {
 		String jpql = "SELECT o FROM User o";
 		TypedQuery<User> query = em.createQuery(jpql, User.class);
 		List<User> list = query.getResultList();
-//		list.forEach(user -> {
-//			String fullname = user.getFullname();
-//			boolean admin = user.getAdmin();
-//			System.out.println(fullname + " " + admin);
-//		});
 		for (User user : list) {
 			String fullname = user.getFullname();
 			boolean admin = user.getAdmin();
@@ -30,21 +27,94 @@ public class UserManager {
 	}
 
 	public void findById() {
-		User user = em.find(User.class, "<<user-id>>");
-		String fullname = user.getFullname();
-		boolean admin = user.getAdmin();
-		System.out.println(fullname + " " + admin);
+		try {
+			System.out.println("Nhập ID: ");
+			String id = scanner.nextLine(); // Nhập ID từ bàn phím
+
+			User user = em.find(User.class, id);
+
+			String fullname = user.getFullname();
+			boolean admin = user.getAdmin();
+			System.out.println(fullname + " " + admin);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 	public void create() {
-		
+		try {
+			System.out.println("Nhập ID: ");
+			String id = scanner.nextLine(); // Nhập ID từ bàn phím
+
+			System.out.println("Nhập Password: ");
+			String password = scanner.nextLine(); // Nhập password từ bàn phím
+
+			System.out.println("Nhập Fullname: ");
+			String fullname = scanner.nextLine(); // Nhập fullname từ bàn phím
+
+			System.out.println("Nhập Email: ");
+			String email = scanner.nextLine(); // Nhập email từ bàn phím
+
+			System.out.println("Nhập Admin (1. true - 0. false): ");
+			String admin = scanner.nextLine(); // Nhập admin từ bàn phím
+
+			User user = new User(id, password, fullname, email, admin.equals("1"));
+			em.getTransaction().begin();
+			em.persist(user);
+			em.getTransaction().commit();
+			System.out.println("Thêm thành công");
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			System.out.println(e);
+		}
 	}
 
 	public void update() {
+		try {
+			System.out.println("Nhập ID: ");
+			String id = scanner.nextLine(); // Nhập ID từ bàn phím
+			User user = em.find(User.class, id);
 
+			System.out.println("Nhập Password mới: ");
+			String password = scanner.nextLine(); // Nhập password từ bàn phím
+			user.setPassword(password);
+
+			System.out.println("Nhập Fullname mới: ");
+			String fullname = scanner.nextLine(); // Nhập fullname từ bàn phím
+			user.setFullname(fullname);
+
+			System.out.println("Nhập Email mới: ");
+			String email = scanner.nextLine(); // Nhập email từ bàn phím
+			user.setEmail(email);
+
+			System.out.println("Nhập Admin (1. true - 0. false): ");
+			String admin = scanner.nextLine(); // Nhập admin từ bàn phím
+			user.setAdmin(admin.equals("1"));
+
+			em.getTransaction().begin();
+			em.merge(user);
+			em.getTransaction().commit();
+			System.out.println("Sửa thành công");
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			System.out.println(e);
+		}
 	}
 
 	public void deleteById() {
+		try {
+			System.out.println("Nhập ID: ");
+			String id = scanner.nextLine(); // Nhập ID từ bàn phím
 
+			User user = em.find(User.class, id);
+
+			em.getTransaction().begin();
+			em.remove(user);
+			em.getTransaction().commit();
+			System.out.println("Xóa thành công");
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			System.out.println(e);
+		}
 	}
 }
--- lab1/src/main/java/model/DbTest.java
+++ lab1/src/main/java/model/DbTest.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Scanner;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,16 +12,50 @@ public class DbTest {
 
 	public static void main(String[] args) {
 		// kết nối csdl
-		// EntityManagerFactory factory = Persistence.createEntityManagerFactory("PolyOE");
-		
+		// EntityManagerFactory factory =
+		// Persistence.createEntityManagerFactory("PolyOE");
+
 		// truy vấn csdl
 		// EntityManager em = factory.createEntityManager();
-		
+
 		// System.out.println("Kết nối db thành công");
-		
+
 		// chạy code UserManager
 		UserManager um = new UserManager();
-		um.finalAll();
+		Scanner scanner = new Scanner(System.in);
+		String choice;
+		while (true) {
+			System.out.println("-------------------------------------------------------");
+			System.out.println("1. Xem danh sách users");
+			System.out.println("2. Tìm user theo ID");
+			System.out.println("3. Thêm user");
+			System.out.println("4. Sửa user");
+			System.out.println("5. Xóa user");
+			System.out.println("0. Thoát");
+			System.out.println("-------------------------------------------------------");
+			choice = scanner.nextLine();
+			switch (choice) {
+			case "1":
+				um.finalAll();
+				break;
+			case "2":
+				um.findById();
+				break;
+			case "3":
+				um.create();
+				break;
+			case "4":
+				um.update();
+				break;
+			case "5":
+				um.deleteById();
+				break;
+			case "0":
+				return;
+			default:
+				break;
+			}
+		}
 	}
-	
+
 }
