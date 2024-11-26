ae6c967bada914063c74de76d34f3151a7122bea
HieuNT-2001 <hieuth0109@gmail.com>
2024-11-26 15:19:28 +0700

    un comment controller

--- lab5/src/main/java/controller/FormController.java
+++ lab5/src/main/java/controller/FormController.java
@@ -2,12 +2,15 @@ package controller;
 
 import java.io.IOException;
 
+import dao.ProductDAO;
 import dao.ProductDetailDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Product;
+import model.ProductDetail;
 
 /**
  * Servlet implementation class BienTheSanPhamController
@@ -41,29 +44,29 @@ public class FormController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// lay du lieu tu jsp
-//		String name = request.getParameter("name");
-//		String description = request.getParameter("description");
-//		String inputPrice = request.getParameter("price"); // Kiem gia rong input chuoi gia ban
-//		int status = Integer.parseInt(request.getParameter("status"));
+		String name = request.getParameter("name");
+		String description = request.getParameter("description");
+		String inputPrice = request.getParameter("price"); // Kiem gia rong input chuoi gia ban
+		int status = Integer.parseInt(request.getParameter("status"));
 		String action = request.getParameter("action");
 
-//		// Kiem tra trong Form
-//		if (name.isBlank() || description.isBlank() || inputPrice.isBlank()) {
-//			return;
-//		}
-//
-//		// chuyen chuoi gia ban sang so
-//		double price = Double.parseDouble(inputPrice);
+		// Kiem tra trong Form
+		if (name.isBlank() || description.isBlank() || inputPrice.isBlank()) {
+			return;
+		}
+
+		// chuyen chuoi gia ban sang so
+		double price = Double.parseDouble(inputPrice);
 
 		// them bien the
-//		if (action.equals("create")) {
-//			// them vao db
-//			ProductDetail entity = new ProductDetail(0, name, description, price, status, ProductDAO.findById(1));
-//			ProductDetailDAO.create(entity);
-//
-//			// reload lai form
-//			response.sendRedirect("./FormController");
-//		}
+		if (action.equals("create")) {
+			// them vao db
+			ProductDetail entity = new ProductDetail(0, name, description, price, status, ProductDAO.findById(1));
+			ProductDetailDAO.create(entity);
+
+			// reload lai form
+			response.sendRedirect("./FormController");
+		}
 
 		// do du lieu tu bang len form
 		if (action.equals("edit")) {
@@ -73,19 +76,19 @@ public class FormController extends HttpServlet {
 		}
 
 		// Update ban ghi trong db
-//		if (action.equals("update")) {
-//			// Lay id tu jsp
-//			int id = Integer.parseInt(request.getParameter("id"));
-//			int productId = Integer.parseInt(request.getParameter("productId"));
-//			Product product = ProductDAO.findById(productId);
-//
-//			// sua trong db
-//			ProductDetail entity = new ProductDetail(id, name, description, price, status, product);
-//			ProductDetailDAO.update(entity);
-//
-//			// reload lai form
-//			response.sendRedirect("./FormController");
-//		}
+		if (action.equals("update")) {
+			// Lay id tu jsp
+			int id = Integer.parseInt(request.getParameter("id"));
+			int productId = Integer.parseInt(request.getParameter("productId"));
+			Product product = ProductDAO.findById(productId);
+
+			// sua trong db
+			ProductDetail entity = new ProductDetail(id, name, description, price, status, product);
+			ProductDetailDAO.update(entity);
+
+			// reload lai form
+			response.sendRedirect("./FormController");
+		}
 
 	}
 
